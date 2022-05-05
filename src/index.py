ece69c5fa026e26a80f9f0a2ecd465828d5a5f09
stevemuss <mus.steven96@gmail.com>
2022-05-04 23:02:38 -0500

    Functional API

--- src/index.py
+++ src/index.py
@@ -2,11 +2,22 @@
 # SPDX-License-Identifier: MIT-0
 
 import boto3
+import json
 
 def lambda_handler(event, context):
   dynamodb_client = boto3.client('dynamodb')
   
-  dynamodb_client.put_item(TableName='WeatherData', Item={'id': {'S': '1'}, 'Weather': {'S': 'Sunny'}})
+  data = json.loads(event.get("body"))
+
+  if data:
+    save_data = {
+        'id': {'S': data.get("name", "")},
+        'recommendation_type': {'S': data.get("recommendation_type", "")},
+        'recommendation': {'S': data.get("recommendation", "")},
+        'python_level': {'S': data.get("python_level", "")},
+    }
+    dynamodb_client.put_item(TableName='WeatherData', Item=save_data)
+  
   return {
       'statusCode': 200,
       'body': 'Successfully inserted data!'
